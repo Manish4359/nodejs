d9d790a2881bce212d10bfd3a17d1ab610f88e93
Manish4359 <manishkr.official4359@gmail.com>
2022-03-04 23:46:50 +0530

    tour page added using pug

--- Natours/controllers/viewsController.js
+++ Natours/controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour=require('./../models/tourModel')
 const catchAsync=require('./../utils/catchAsync')
+const url=require('url')
 
 exports.getOverview =catchAsync(async (req, res) => {
 
@@ -11,13 +12,20 @@ exports.getOverview =catchAsync(async (req, res) => {
 
     //render
     res.status(200).render('overview', {
-        title: 'all tours',
+        title: `Exciting tours for adventurous people`,
         tours
     })
 })
 
-exports.getTour = (req, res) => {
+exports.getTour =catchAsync(async (req, res) => {
+
+   const tour=await Tour.findOne({slug:req.params.slug}).populate({
+       path:'reviews',
+       fields:'review rating user'
+   });
+
     res.status(200).render('tour', {
-        title: ' The Forest Hiker'
+        title: tour.name,
+        tour
     })
-}
+});
