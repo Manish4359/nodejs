c9b45fe3bf03eeee4a20ff3499f5282db8b0ac43
Manish4359 <manishkr.official4359@gmail.com>
2022-03-11 16:26:38 +0530

    integrating stripe into the backend

--- Natours/app.js
+++ Natours/app.js
@@ -15,6 +15,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const { dirname } = require('path');
 
 const app = express();
@@ -96,6 +97,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   // const err = new Error(`can't find ${req.originalUrl} on the server`);
